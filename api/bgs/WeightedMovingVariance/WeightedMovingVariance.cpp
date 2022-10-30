9896ef844c2cee84dec8fe0ed014e25a74ab2468
Fabio Barros <fabio.c.barros@gmail.com>
2022-10-30 15:32:28 -0300

    Treating variavle passing and constants

--- api/bgs/WeightedMovingVariance/WeightedMovingVariance.cpp
+++ api/bgs/WeightedMovingVariance/WeightedMovingVariance.cpp
@@ -8,56 +8,49 @@
 using namespace sky360lib::bgs;
 
 WeightedMovingVariance::WeightedMovingVariance(bool _enableWeight,
-                                    bool _enableThreshold,
-                                    int _threshold,
-                                    size_t _numProcessesParallel) 
-    : CoreBgs(_numProcessesParallel)
-    , m_params(_enableWeight, _enableThreshold, _threshold)
+                                               bool _enableThreshold,
+                                               float _threshold,
+                                               size_t _numProcessesParallel)
+    : CoreBgs(_numProcessesParallel),
+      m_params(_enableWeight, _enableThreshold, _threshold,
+               _enableWeight ? DEFAULT_WEIGHTS[0] : ONE_THIRD,
+               _enableWeight ? DEFAULT_WEIGHTS[1] : ONE_THIRD,
+               _enableWeight ? DEFAULT_WEIGHTS[2] : ONE_THIRD)
 {
     imgInputPrevParallel.resize(m_numProcessesParallel);
 
-    for (int i = 0; i < m_numProcessesParallel; ++i) {
-        m_processSeq.push_back(i);
+    for (int i = 0; i < m_numProcessesParallel; ++i)
+    {
         imgInputPrevParallel[i][0] = std::make_unique<cv::Mat>();
         imgInputPrevParallel[i][1] = std::make_unique<cv::Mat>();
     }
 }
 
-WeightedMovingVariance::~WeightedMovingVariance() {
+WeightedMovingVariance::~WeightedMovingVariance()
+{
 }
 
-void WeightedMovingVariance::getBackgroundImage(cv::Mat& _bgImage) {
+void WeightedMovingVariance::getBackgroundImage(cv::Mat &_bgImage)
+{
 }
 
-void WeightedMovingVariance::initialize(const cv::Mat& _image) {
+void WeightedMovingVariance::initialize(const cv::Mat &_image)
+{
 }
 
-void WeightedMovingVariance::process(const cv::Mat &_imgInput, cv::Mat &_imgOutput, int _numProcess) {
-    if (_imgOutput.empty()) {
+void WeightedMovingVariance::process(const cv::Mat &_imgInput, cv::Mat &_imgOutput, int _numProcess)
+{
+    if (_imgOutput.empty())
+    {
         _imgOutput.create(_imgInput.size(), CV_8UC1);
     }
     process(_imgInput, _imgOutput, imgInputPrevParallel[_numProcess], m_params);
 }
 
-// void WeightedMovingVariance::processParallel(const cv::Mat &_imgInput, cv::Mat &_imgOutput) {
-//     std::for_each(
-//         std::execution::par,
-//         m_processSeq.begin(),
-//         m_processSeq.end(),
-//         [&](int np)
-//         {
-//             const int height{_imgInput.size().height / m_numProcessesParallel};
-//             const int pixelPos{np * _imgInput.size().width * height};
-//             cv::Mat imgSplit(height, _imgInput.size().width, _imgInput.type(), _imgInput.data + (pixelPos * _imgInput.channels()));
-//             cv::Mat maskPartial(height, _imgInput.size().width, _imgOutput.type(), _imgOutput.data + pixelPos);
-//             process(imgSplit, maskPartial, imgInputPrevParallel[np], m_params);
-//         });
-// }
-
-void WeightedMovingVariance::process(const cv::Mat &_inImage, 
-                                    cv::Mat &_outImg, 
-                                    std::array<std::unique_ptr<cv::Mat>, 2>& _imgInputPrev, 
-                                    const WeightedMovingVarianceParams& _params)
+void WeightedMovingVariance::process(const cv::Mat &_inImage,
+                                     cv::Mat &_outImg,
+                                     std::array<std::unique_ptr<cv::Mat>, 2> &_imgInputPrev,
+                                     const WeightedMovingVarianceParams &_params)
 {
     auto inImageCopy = std::make_unique<cv::Mat>();
     _inImage.copyTo(*inImageCopy);
@@ -84,34 +77,32 @@ void WeightedMovingVariance::process(const cv::Mat &_inImage,
     _imgInputPrev[0] = std::move(inImageCopy);
 }
 
-inline float calcWeightedVariance(const uchar* const i1, const uchar* const i2, const uchar* const i3,
-                        const float w1, const float w2, const float w3) {
+inline float calcWeightedVariance(const uchar *const i1, const uchar *const i2, const uchar *const i3,
+                                  const WeightedMovingVarianceParams &_params)
+{
     const float dI[] = {(float)*i1, (float)*i2, (float)*i3};
-    const float mean{(dI[0] * w1) + (dI[1] * w2) + (dI[2] * w3)};
+    const float mean{(dI[0] * _params.weight1) + (dI[1] * _params.weight2) + (dI[2] * _params.weight3)};
     const float value[] = {dI[0] - mean, dI[1] - mean, dI[2] - mean};
-    return std::sqrt(((value[0] * value[0]) * w1) + ((value[1] * value[1]) * w2) + ((value[2] * value[2]) * w3));
-} 
+    return std::sqrt(((value[0] * value[0]) * _params.weight1) + ((value[1] * value[1]) * _params.weight2) + ((value[2] * value[2]) * _params.weight3));
+}
 
 void WeightedMovingVariance::weightedVarianceMono(
-        const cv::Mat &img1, 
-        const cv::Mat &img2, 
-        const cv::Mat &img3, 
-        cv::Mat& outImg,
-        const WeightedMovingVarianceParams& _params)
+    const cv::Mat &img1,
+    const cv::Mat &img2,
+    const cv::Mat &img3,
+    cv::Mat &outImg,
+    const WeightedMovingVarianceParams &_params)
 {
-    const float weight1{_params.enableWeight ? 0.5f : ONE_THIRD}; 
-    const float weight2{_params.enableWeight ? 0.3f : ONE_THIRD}; 
-    const float weight3{_params.enableWeight ? 0.2f : ONE_THIRD}; 
-
     size_t totalDataSize{(size_t)img1.size().area()};
     uchar *dataI1{img1.data};
     uchar *dataI2{img2.data};
     uchar *dataI3{img3.data};
     uchar *dataOut{outImg.data};
-    for (size_t i{0}; i < totalDataSize; ++i) {
-        float result{calcWeightedVariance(dataI1, dataI2, dataI3, weight1, weight2, weight3)};
-        *dataOut = _params.enableThreshold ? ((uchar)(result > _params.threshold ? 255 : 0))
-                                            : (uchar)result;
+    for (size_t i{0}; i < totalDataSize; ++i)
+    {
+        float result{calcWeightedVariance(dataI1, dataI2, dataI3, _params)};
+        *dataOut = _params.enableThreshold ? ((uchar)(result > _params.threshold ? 255.0f : 0.0f))
+                                           : (uchar)result;
         ++dataOut;
         ++dataI1;
         ++dataI2;
@@ -120,15 +111,12 @@ void WeightedMovingVariance::weightedVarianceMono(
 }
 
 void WeightedMovingVariance::weightedVarianceColor(
-        const cv::Mat &img1, 
-        const cv::Mat &img2, 
-        const cv::Mat &img3, 
-        cv::Mat& outImg,
-        const WeightedMovingVarianceParams& _params)
+    const cv::Mat &img1,
+    const cv::Mat &img2,
+    const cv::Mat &img3,
+    cv::Mat &outImg,
+    const WeightedMovingVarianceParams &_params)
 {
-    const float weight1{_params.enableWeight ? 0.5f : ONE_THIRD}; 
-    const float weight2{_params.enableWeight ? 0.3f : ONE_THIRD}; 
-    const float weight3{_params.enableWeight ? 0.2f : ONE_THIRD}; 
     const int numChannels = img1.channels();
 
     size_t totalDataSize{(size_t)img1.size().area()};
@@ -136,13 +124,14 @@ void WeightedMovingVariance::weightedVarianceColor(
     uchar *dataI2{img2.data};
     uchar *dataI3{img3.data};
     uchar *dataOut{outImg.data};
-    for (size_t i{0}; i < totalDataSize; ++i) {
-        const float r{calcWeightedVariance(dataI1, dataI2, dataI3, weight1, weight2, weight3)};
-        const float g{calcWeightedVariance(dataI1 + 1, dataI2 + 1, dataI3 + 1, weight1, weight2, weight3)};
-        const float b{calcWeightedVariance(dataI1 + 2, dataI2 + 2, dataI3 + 2, weight1, weight2, weight3)};
+    for (size_t i{0}; i < totalDataSize; ++i)
+    {
+        const float r{calcWeightedVariance(dataI1, dataI2, dataI3, _params)};
+        const float g{calcWeightedVariance(dataI1 + 1, dataI2 + 1, dataI3 + 1, _params)};
+        const float b{calcWeightedVariance(dataI1 + 2, dataI2 + 2, dataI3 + 2, _params)};
         const float result{0.299f * r + 0.587f * g + 0.114f * b};
-        *dataOut = _params.enableThreshold ? ((uchar)(result > _params.threshold ? 255 : 0))
-                                            : (uchar)result;
+        *dataOut = _params.enableThreshold ? ((uchar)(result > _params.threshold ? 255.0f : 0.0f))
+                                           : (uchar)result;
         ++dataOut;
         dataI1 += numChannels;
         dataI2 += numChannels;
--- api/bgs/WeightedMovingVariance/WeightedMovingVariance.hpp
+++ api/bgs/WeightedMovingVariance/WeightedMovingVariance.hpp
@@ -7,42 +7,55 @@
 #include <array>
 #include <vector>
 
-namespace sky360lib::bgs {
-    
+namespace sky360lib::bgs
+{
     struct WeightedMovingVarianceParams
     {
         WeightedMovingVarianceParams(bool _enableWeight,
-                                    bool _enableThreshold,
-                                    int _threshold)
+                                     bool _enableThreshold,
+                                     float _threshold,
+                                     float _weight1,
+                                     float _weight2,
+                                     float _weight3)
             : enableWeight(_enableWeight),
-            enableThreshold(_enableThreshold),
-            threshold(_threshold)
-        {}
+              enableThreshold(_enableThreshold),
+              threshold(_threshold),
+              weight1(_weight1),
+              weight2(_weight2),
+              weight3(_weight3)
+        {
+        }
 
         const bool enableWeight;
         const bool enableThreshold;
-        const int threshold;
+        const float threshold;
+
+        const float weight1;
+        const float weight2;
+        const float weight3;
     };
 
     class WeightedMovingVariance
-        : public CoreBgs {
+        : public CoreBgs
+    {
     public:
-        static const bool DEFAULT_ENABLE_WEIGHT{true};
-        static const bool DEFAULT_ENABLE_THRESHOLD{true};
-        static const int DEFAULT_THRESHOLD_VALUE{15};
+        static inline const bool DEFAULT_ENABLE_WEIGHT{true};
+        static inline const bool DEFAULT_ENABLE_THRESHOLD{true};
+        static inline const float DEFAULT_THRESHOLD_VALUE{15.0f};
         /// defines the default value for the number of parallel threads
-        static const size_t DEFAULT_PARALLEL_TASKS{12};
+        static inline const size_t DEFAULT_PARALLEL_TASKS{12};
+        static inline const float DEFAULT_WEIGHTS[] = {0.5f, 0.3f, 0.2f};
 
         WeightedMovingVariance(bool _enableWeight = DEFAULT_ENABLE_WEIGHT,
                                bool _enableThreshold = DEFAULT_ENABLE_THRESHOLD,
-                               int _threshold = DEFAULT_THRESHOLD_VALUE,
+                               float _threshold = DEFAULT_THRESHOLD_VALUE,
                                size_t _numProcessesParallel = DEFAULT_PARALLEL_TASKS);
         ~WeightedMovingVariance();
 
-        void getBackgroundImage(cv::Mat& _bgImage);
+        void getBackgroundImage(cv::Mat &_bgImage);
 
     private:
-        void initialize(const cv::Mat& _image);
+        void initialize(const cv::Mat &_image);
         void process(const cv::Mat &img_input, cv::Mat &img_output, int _numProcess);
 
         static inline const float ONE_THIRD{1.0f / 3.0f};
@@ -50,22 +63,21 @@ namespace sky360lib::bgs {
         std::vector<std::array<std::unique_ptr<cv::Mat>, 2>> imgInputPrevParallel;
         const WeightedMovingVarianceParams m_params;
 
-        //void processParallel(const cv::Mat &_imgInput, cv::Mat &_imgOutput);
-        static void process(const cv::Mat &img_input, 
-                            cv::Mat &img_output, 
-                            std::array<std::unique_ptr<cv::Mat>, 2>& img_input_prev, 
-                            const WeightedMovingVarianceParams& _params);
+        static void process(const cv::Mat &img_input,
+                            cv::Mat &img_output,
+                            std::array<std::unique_ptr<cv::Mat>, 2> &img_input_prev,
+                            const WeightedMovingVarianceParams &_params);
         static void weightedVarianceMono(
-                const cv::Mat &_img1, 
-                const cv::Mat &_img2, 
-                const cv::Mat &_img3, 
-                cv::Mat& _outImg,
-                const WeightedMovingVarianceParams& _params);
+            const cv::Mat &_img1,
+            const cv::Mat &_img2,
+            const cv::Mat &_img3,
+            cv::Mat &_outImg,
+            const WeightedMovingVarianceParams &_params);
         static void weightedVarianceColor(
-                const cv::Mat &_img1, 
-                const cv::Mat &_img2, 
-                const cv::Mat &_img3, 
-                cv::Mat& _outImg,
-                const WeightedMovingVarianceParams& _params);
+            const cv::Mat &_img1,
+            const cv::Mat &_img2,
+            const cv::Mat &_img3,
+            cv::Mat &_outImg,
+            const WeightedMovingVarianceParams &_params);
     };
 }
